9774b29227d08da71d939649c23af9d96fe2437e
Akshay <akshay23214@gmail.com>
2026-07-01 19:31:28 +0530

    configured the Database connection using mongoosegit add .

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv"
+import { app } from "./app.js"
+import connectDB from "./db/index.js"
+
+
+dotenv.config()
+
+
+
+connectDB()
+.then( () => {
+    try {
+        app.listen( process.env.PORT, () => {
+            console.log(" App is running on port: ", process.env.PORT)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
+.catch(err){
+    console.log("DataBase connection failed! ", err)
+}
